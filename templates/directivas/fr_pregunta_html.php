58e2014cb51a6e626dd45b1e397f5b4cc99ac3da
mgamboa <gamboa.martin@gmail.com>
2022-07-01 17:06:55 -0500

    v0.1.0 Se inicializa paquete

---
+++ templates/directivas/fr_pregunta_html.php
@@ -0,0 +1,10 @@
+<?php
+namespace html;
+
+use gamboamartin\system\html_controler;
+
+
+class fr_pregunta_html extends html_controler {
+
+
+}
---
+++ templates/directivas/fr_tipo_pregunta_html.php
@@ -0,0 +1,10 @@
+<?php
+namespace html;
+
+use gamboamartin\system\html_controler;
+
+
+class fr_tipo_pregunta_html extends html_controler {
+
+
+}
